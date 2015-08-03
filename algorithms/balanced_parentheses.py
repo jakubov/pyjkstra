6284cab60d98b56c46126d8d5844a0086068b96a
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-08-03 15:49:38 -0700

    Add Balance Parentheses with tests

---
+++ algorithms/balanced_parentheses.py
@@ -0,0 +1,17 @@
+from pyjkstra.data_structures.stack import Stack
+
+class BalancedParentheses:
+
+    def is_balanced(self, word):
+        stack = Stack()
+        balanced = True
+        for c in word:
+            if c == '(':
+                stack.push(c)
+            else:
+                if stack.is_empty():
+                    balanced = False
+                else:
+                    stack.pop()
+
+        return balanced and stack.is_empty()
---
+++ tests/balanced_parentheses_tests.py
@@ -0,0 +1,32 @@
+import unittest
+
+from pyjkstra.algorithms.balanced_parentheses import BalancedParentheses
+
+BALANCED_STRINGS = [
+    '()',
+    '((()))',
+    '(()()()())',
+    '(()((())()))',
+    '(()((())()))()()'
+]
+
+UNBALANCED_STRINGS = [
+    '(',
+    ')(',
+    '()))',
+    '(()()(()',
+    '((((((((())'
+]
+
+bp = BalancedParentheses()
+
+
+class BalancedParenthesesTestCase(unittest.TestCase):
+
+
+    def test_is_balanced(self):
+        for s in BALANCED_STRINGS:
+            self.assertTrue(bp.is_balanced(s))
+
+        for s in UNBALANCED_STRINGS:
+            self.assertFalse(bp.is_balanced(s))
